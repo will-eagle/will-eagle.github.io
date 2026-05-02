5cdf9acf7274f05baf9a73a39214d3fa654cb9d5
will-eagle <will@texaseagles.net>
2026-05-02 20:21:37 +0000

    Deploying to gh-pages from @ will-eagle/will-eagle.github.io@16e71afb6f956376784c45c6d6991db9e051b7b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -226,17 +226,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/post-citation/";
           
         },
-      },{id: "post-a-post-with-pseudo-code",
-        
-          title: "a post with pseudo code",
-        
-        description: "this is what included pseudo code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/pseudocode/";
-          
-        },
       },{id: "post-2024-udall-scholar",
         
           title: "2024 Udall Scholar",
