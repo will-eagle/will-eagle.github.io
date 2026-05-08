37ee3081353e49537717ce2187adbdc3f80c5898
will-eagle <will@texaseagles.net>
2026-05-08 00:13:32 +0000

    Deploying to gh-pages from @ will-eagle/will-eagle.github.io@bbf4732cfbc82779e9ef03757c6b15eb459554f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,14 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
+        },{id: "dropdown-blog",
               title: "blog",
               description: "",
               section: "Dropdown",
